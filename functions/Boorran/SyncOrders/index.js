93c95f32ae8a0980c743e5dc4d7ec936955132a8
Mike Van <miike.vaan@gmail.com>
2020-01-28 16:54:15 +0700

    query string generation

--- functions/Boorran/SyncOrders/index.js
+++ functions/Boorran/SyncOrders/index.js
@@ -4,7 +4,17 @@ const { GET } = require('fetchier');
 const { BOORRAN_API_KEY, BOORRAN_API_PW, BOORRAN_STORE } = require('../../../config.js');
 
 module.exports.handler = async event => {
-  GET({
-    url: `https://${BOORRAN_API_KEY}:${BOORRAN_API_PW}@${BOORRAN_STORE}/admin/api/2020-01/orders.json?limit=2&status=any`
-  }).then(console.log);
+  try {
+    const params = event.queryParams || {};
+    const qs = Object.keys(params).length
+              && Object.keys(params)
+                .filter(key => params[key] || params[key].length)
+                .map(key => `${key}=${params[key]}`)
+                .join('&') || 'limit=20';
+
+    const res = await GET({ debug: true, url: `https://${BOORRAN_API_KEY}:${BOORRAN_API_PW}@${BOORRAN_STORE}/admin/api/2020-01/orders.json?${qs}` });
+    return success(res);
+  } catch (err) {
+    return fail(err);
+  }
 };
